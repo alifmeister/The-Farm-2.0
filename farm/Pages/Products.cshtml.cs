684b81c79ad54b0990440f41426c782cdfc59a05
Zdeněk Pašek <me@zdenekpasek.cz>
2020-12-08 05:25:37 +0100

    add front-end

---
+++ farm/Pages/Products.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace farm.Pages
+{
+    public class ProductsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
